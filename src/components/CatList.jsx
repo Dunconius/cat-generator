64a80bff114b62a70d3e761959a5848058d7efdc
Dunconius <duncanbean84@gmail.com>
2024-05-30 16:47:50 +1000

    fix: random names and limit to 6 results

--- src/components/CatList.jsx
+++ src/components/CatList.jsx
@@ -10,18 +10,45 @@ let catList = [
     "Fluffy",
     "Scruffy",
     "Puffy",
-    "Diddy",
-    "Kitty",
-    "Iffy",
+    "Buttermilk",
+    "Crockett",
+    "Trouble",
+    "TomTom",
+    "Squeaker",
+    "Banshee",
+    "Jones",
+    "Mr Jenkins",
+    "Scruff McGraw",
+    "Ozzy",
+    "Mrs Whiskers",
+    "Truffles",
+    "Trombone",
+    "Pork Chop",
+    "Kittykins",
 ];
 
+const shuffleArray = (array) => {
+  let shuffledArray = array.slice(); // Copy the array
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+};
 
 export default function CatList() {
     const [catImages, setCatImages] = useState([]);
+    const [randomCatNames, setRandomCatNames] = useState([]);
   
     const handleFetchCatImages = async () => {
-      const images = await Promise.all(catList.map(() => fetchCatImage()));
+      // Fetch 6 cat images
+      const images = await Promise.all(Array.from({ length: 6 }, () => fetchCatImage()));
+
+      // Shuffle the cat names and take the first 6
+      const shuffledCatNames = shuffleArray(catList).slice(0, 6);
+      
       setCatImages(images);
+      setRandomCatNames(shuffledCatNames.slice(0, images.length));
     };
   
     return (
@@ -29,9 +56,9 @@ export default function CatList() {
         <h1>Cat Generator</h1>
         <button onClick={handleFetchCatImages}>Get Cats!</button>
         <div className="catList">
-          {catList.map((cat, index) => (
-            <CatCard key={index} catName={cat} catImage={catImages[index]} />
-          ))}
+        {catImages.map((image, index) => (
+        <CatCard key={index} catName={randomCatNames[index]} catImage={image} />
+      ))}
         </div>
       </section>
     );
